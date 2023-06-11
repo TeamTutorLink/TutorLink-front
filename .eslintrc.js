111693c45a940fc131ddab717501badb47ae39c1
hajiung25 <hajiung25@gmail.com>
2023-06-11 18:34:57 +0900

    fix: eslint, typescript 연동 오류 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,18 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
+  parser: '@typescript-eslint/parser',
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest'
   },
   plugins: [
-    'react'
+    'react',
+    '@typescript-eslint',
   ],
   rules: {
   }
