e0320ca5390c49b0b94484bd57902865c85f43b8
hajiung25 <hajiung25@gmail.com>
2023-06-19 11:40:00 +0900

    feat: 이미지 추가 및 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SettingTest from './SettingTest';
 import MainPage from './pages/Home/MainPage';
+import LoginPage from './pages/Login/LoginPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/test" element={<SettingTest />} />
       </Routes>
     </BrowserRouter>
