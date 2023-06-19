bcab9c765d8dfd6cc85d6e5ccbca451fb26d8acc
danakim530 <danakim530@gmail.com>
2023-06-20 01:02:04 +0900

    feat: create floating button structure

---
+++ src/components/FloatingButton.tsx
@@ -0,0 +1,50 @@
+import styled, { css } from 'styled-components';
+import React from 'react';
+import { useState } from 'react';
+
+const FloatingButton = () => {
+  const [toggle, setToggle] = useState(false);
+  const clickedToggle = () => {
+    setToggle((prev) => !prev);
+  };
+  return (
+    <div>
+      <StyledCircleDiv onClick={clickedToggle}>+</StyledCircleDiv>
+      <StyledSideBlock toggle={toggle}>강의 추가 하기</StyledSideBlock>
+    </div>
+  );
+};
+
+const StyledCircleDiv = styled.div`
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+  background-color: #0e0171;
+  color: white;
+  font-size: 56px;
+  padding-left: 11px;
+  position: fixed;
+  z-index: 10;
+  right: 40px;
+  bottom: 40px;
+  cursor: pointer;
+`;
+
+const StyledSideBlock = styled.div`
+  position: absolute;
+  position: fixed;
+  right: -70px;
+  font-weight: bold;
+  bottom: 45px;
+  background-color: #0cdcbf;
+  padding: 16px 34px 16px 30px;
+  border-radius: 20px;
+  ${(props) =>
+    props.toggle &&
+    css`
+      transform: translate(-142px, 0);
+      transition: all 0.5s ease-in-out;
+    `}
+`;
+
+export default FloatingButton;
--- src/pages/Home/MainPage.tsx
+++ src/pages/Home/MainPage.tsx
@@ -2,6 +2,7 @@ import Header from '../../components/common/Header';
 import Footer from '../../components/common/Footer';
 import React from 'react';
 import styled from 'styled-components';
+import FloatingButton from '../../components/FloatingButton';
 
 const MainPage = () => {
   return (
@@ -17,6 +18,17 @@ const MainPage = () => {
         <div>00개의 강의를 찾았어요!</div>
         <hr />
       </div>
+      <div>
+        <div>관련된 강의</div>
+        <div>
+          00개의 강의를 찾았어요! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Corrupti odit voluptatem aut doloribus numquam eos
+          nisi, ad quam consectetur impedit in, alias maxime. Veritatis
+          voluptate quo dolores dolore pariatur quaerat!
+        </div>
+        <hr />
+      </div>
+      <FloatingButton />
       <Footer />
     </div>
   );
