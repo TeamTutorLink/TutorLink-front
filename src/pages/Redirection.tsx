1efc1843016492a7433eea82f16d03b277be8e1d
danakim530 <danakim530@gmail.com>
2023-07-22 00:41:59 +0900

    fix: .env + Redirect uri : 1 stage Complete

--- src/pages/Redirection.tsx
+++ src/pages/Redirection.tsx
@@ -8,20 +8,30 @@ import axios from 'axios';
 
 // 카카오 로그인 하면, 주소창의 URI 뒤에 파라미터로 ?code=@@@@@@@ 인가 코드 넘어옴
 const Redirection = () => {
-  const code = window.location.search; //해당 주소에서 search키에 담긴 코드 받아오기
+  //인가코드 받기
+  const code = new URL(window.location.href).searchParams.get('code'); //해당 주소에서 search키에 담긴 코드 받아오기
   const navigate = useNavigate();
 
   useEffect(() => {
     console.log(process.env.REDIRECT_URI);
-    axios.post(`${process.env.REDIRECT_URI}kakaoLogin${code}`).then((r) => {
-      //백엔드로 인가코드 전달
-      console.log(r.data); // 토큰과 함께 오는 정보 출력
+    axios
+      .post('http://15.164.165.34:8080/auth/kakao-login', {
+        code: `${code}`,
+      })
+      .then((r) => {
+        //백엔드로 인가코드 전달
+        console.log(r.data); // 토큰과 함께 오는 정보 출력
 
-      // 토큰을 받아서 localStorage같은 곳에 저장하는 코드
-      localStorage.setItem('accessToken', r.data.accessToken); // 일단 이름만 저장
-      localStorage.setItem('refreshToken', r.data.refreshToken);
-      navigate('/');
-    });
+        // 토큰을 받아서 localStorage같은 곳에 저장하는 코드
+        localStorage.setItem('accessToken', r.data.accessToken); // 일단 이름만 저장
+        localStorage.setItem('refreshToken', r.data.refreshToken);
+        navigate('/');
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log('ERROR: ', error.response.data.responseMessage);
+        alert(error.response.data.responseMessage);
+      });
   }, []);
 
   return <div>로그인 중입니다.</div>;
